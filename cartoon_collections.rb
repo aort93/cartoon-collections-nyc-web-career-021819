cfac4a9f05f70c1c9fd3b3700f2046b59ae55cc4
Alonso Ortega <alonsojortega@gmail.com>
2019-01-28 02:39:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,10 @@ def long_planeteer_calls(words)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  strings.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
